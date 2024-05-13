865c48556391576e65745718cee0a7afdcc40f7c
NTT102 <53190789+NTT102@users.noreply.github.com>
2024-05-14 02:33:28 +0700

    Update Guide.tsx

--- src/layouts/sections/Guide.tsx
+++ src/layouts/sections/Guide.tsx
@@ -132,7 +132,35 @@ const Guide = () => (
                                 <>
                                     <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
                                         <span className="block text-base first-letter:uppercase font-semibold">
-                                            1. Voucher SEED có hạn sử dụng không ạ?
+                                            Opp dự án HorusLayer
+                                        </span>
+                                        <IoIosArrowDown
+                                            className={`${
+                                                open
+                                                    ? "rotate-180 transform"
+                                                    : ""
+                                            } h-5 w-5 text-black`}
+                                        />
+                                    </Disclosure.Button>
+                                     <Disclosure.Panel className="text-sm text-gray-500">
+                                        <iframe
+                                            className="w-full aspect-video"
+                                            src="https://www.youtube.com"
+                                            title="YouTube video player"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        ></iframe>
+                                    </Disclosure.Panel>
+                                </>
+                            )}
+                        </Disclosure>
+                    </Tab.Panel>
+                     <Tab.Panel className="grid gap-2">
+                        <Disclosure>
+                            {({ open }) => (
+                                <>
+                                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
+                                        <span className="block text-base first-letter:uppercase font-semibold">
+                                            1. Contract TOKEN
                                         </span>
                                         <IoIosArrowDown
                                             className={`${
@@ -144,12 +172,20 @@ const Guide = () => (
                                     </Disclosure.Button>
                                     <Disclosure.Panel className="text-sm text-gray-500">
                                         <p className="text-white font-medium first-letter:uppercase">
-                                            Trả lời : Không, nó có thể được sử dụng làm Voucher giảm giá $250 cho NFT mới !
+                                            Contract HRX mạng BSC : 0x8A3C92a7A7Aa9eed4d390aAD1708dD5995720dAe
+                                        </p>
+                                         <p className="text-white font-medium first-letter:uppercase">
+                                            Contract USDT mạng BSC : 0x55d398326f99059fF775485246999027B3197955
+                                        </p>
+                                         <p className="text-white font-medium first-letter:uppercase">
+                                            Contract ETH mạng BSC : 0x2170Ed0880ac9A755fd29B2688956BD959F933F8
                                         </p>
                                     </Disclosure.Panel>
                                 </>
                             )}
                         </Disclosure>
+                         
+                        
                     </Tab.Panel>
                 </Tab.Panels>
             </Tab.Group>
