fefc84e7529f0333b4bb53d300b6a0903b11531e
NTT102 <53190789+NTT102@users.noreply.github.com>
2024-05-28 23:21:31 +0700

    Update CourseTabs.tsx

--- src/components/CourseTabs.tsx
+++ src/components/CourseTabs.tsx
@@ -32,6 +32,16 @@ const CourseTabs = () => {
                         return "text-gray-400 text-lg font-semibold border-b border-transparent outline-none first-letter:uppercase py-2";
                     }}
                 >
+                    Sự kiện
+                </Tab>
+                <Tab
+                    className={({ selected }) => {
+                        if (selected)
+                            return "text-white text-lg font-semibold border-b border-white outline-none first-letter:uppercase py-2";
+
+                        return "text-gray-400 text-lg font-semibold border-b border-transparent outline-none first-letter:uppercase py-2";
+                    }}
+                >
                     Các câu hỏi (Q&A)
                 </Tab>
                
