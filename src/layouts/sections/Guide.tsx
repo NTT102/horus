5e4ccfd0f5054ffe373417e0cee26e6b68ed7c94
NTT102 <53190789+NTT102@users.noreply.github.com>
2024-05-28 23:33:07 +0700

    Update Guide.tsx

--- src/layouts/sections/Guide.tsx
+++ src/layouts/sections/Guide.tsx
@@ -212,6 +212,36 @@ const Guide = () => (
                                 <>
                                     <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
                                         <span className="block text-base first-letter:uppercase font-semibold">
+                                            Sự kiện Private Party của Long Capital | 25/05/2024
+                                        </span>
+                                        <IoIosArrowDown
+                                            className={`${
+                                                open
+                                                    ? "rotate-180 transform"
+                                                    : ""
+                                            } h-5 w-5 text-black`}
+                                        />
+                                    </Disclosure.Button>
+                                     <Disclosure.Panel className="text-sm text-gray-500">
+                                        <iframe
+                                            className="w-full aspect-video"
+                                            src="https://www.youtube.com/embed/w74A9cfUH6Q?si=NLOx7ALVIaGdVGxy"
+                                            title="YouTube video player"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        ></iframe>
+                                    </Disclosure.Panel>
+                                </>
+                            )}
+                        </Disclosure>
+                       
+                         
+                    </Tab.Panel>
+                     <Tab.Panel className="grid gap-2">
+                        <Disclosure>
+                            {({ open }) => (
+                                <>
+                                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
+                                        <span className="block text-base first-letter:uppercase font-semibold">
                                             1. Contract TOKEN
                                         </span>
                                         <IoIosArrowDown
